85ef3ca44745b9654a8262cb282bcdf7f1d6b19b
jamesvincent <jamesvincent@users.noreply.github.com>
2023-12-20 14:30:23 +0000

    Update RunSpodcast.ps1

--- RunSpodcast.ps1
+++ RunSpodcast.ps1
@@ -2,16 +2,15 @@
 $Stamp = (Get-Date).toString("yyyy-MM-dd__ HH-mm-ss")
 Start-Transcript -Path "C:\ProgramData\spodcast\html\Spodcast_$Stamp.log" -Append
 Write-Output "Check for older files and remove them"
-Get-ChildItem -Path  "C:\ProgramData\spodcast\html" -Recurse -include *.log | Where-Object { $_.CreationTime -lt (Get-Date).AddDays(-14) } | Remove-Item
 Write-Output "Stop any existing/stale containers"
-docker stop $(docker ps -a -q)
+docker ps -q | % { docker stop $_ }
 docker container prune -f
 Write-Output "Check for new Image updates"
 docker pull heywoodlh/spodcast:latest
 docker pull heywoodlh/spodcast-web:latest
 docker pull heywoodlh/spodcast-cron:latest
 Write-Output "Run Spodcast and check for latest episodes"
-docker run --rm -it -v c:\ProgramData\spodcast:/data heywoodlh/spodcast -c /data/spodcast.json --log-level debug --transcode yes --root-path /data/html --rss-feed yes --max-episodes 8 https://open.spotify.com/show/XXXSHOWIDHEREXXX https://open.spotify.com/show/XXXSHOWIDHEREXXX
+docker run --rm -it -v c:\ProgramData\spodcast:/data heywoodlh/spodcast -c /data/spodcast.json --log-level debug --transcode yes --root-path /data/html --rss-feed yes --max-episodes 8 https://open.spotify.com/show/INSERTSHOWIDHERE https://open.spotify.com/show/INSERTSHOWIDHERE
 docker container prune -f
 Write-Output "Complete"
 Write-Output "Killing Docker Desktop Sessions"
